91916c0eb7d3a372e0de6dd259d2bf4be32c0c9a
MateusWill <matilegal@gmail.com>
2020-06-26 15:42:08 -0300

    Mapeiei essa Benção

---
+++ Modelos/filmedbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SFive.Modelos
+{
+    public partial class filmedbContext : DbContext
+    {
+        public filmedbContext()
+        {
+        }
+
+        public filmedbContext(DbContextOptions<filmedbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TbFilme> TbFilme { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("server=localhost;user id=root;password=123456;database=filmedb", x => x.ServerVersion("8.0.20-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TbFilme>(entity =>
+            {
+                entity.HasKey(e => e.IdFilme)
+                    .HasName("PRIMARY");
+
+                entity.Property(e => e.DsGenero)
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.NmFilme)
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
